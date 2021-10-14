ff46a468d548995d8add68b341ec4809d499865e
Perry Northcutt <perry85dallas@gmail.com>
2021-10-14 16:47:13 -0500

    code for index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// npm init -y starts an npm project, creates a package.json
+// npm install inquirer
+// start coding
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require()
