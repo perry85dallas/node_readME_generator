1d3bc6870bccfa503cd4382a65ce5ae69e55f002
Perry Northcutt <perry85dallas@gmail.com>
2021-10-21 14:47:41 -0500

    updates to js

--- index.js
+++ index.js
@@ -4,4 +4,82 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require()
+const generateMarkdown = require('./util/generateMarkdown');
+
+// user prompt with questions for readme using promptUser and inquirer
+
+function promptUser(){
+    inquirer.prompt([
+
+        {
+            type: 'input',
+            name:'title',
+            message:'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name:'description',
+            message:'Please write a description of your project?'
+        },
+        {
+            type: 'input',
+            name:'installation',
+            message:'What are your installation instructions?'
+        },
+        {
+            type: 'input',
+            name:'usage',
+            message:'What is this project used for?'
+        },
+        {
+            type: 'list',
+            name:'license',
+            message:'Please select the license you would like to use.'
+            choices: [
+                'MIT License',
+                'Apache License',
+                'GPL General Public License',
+            ]
+        },
+        {
+            type: 'input',
+            name:'contributors',
+            message:'Please add any contributors to your project.'
+        },
+        {
+            type: 'input',
+            name:'testing',
+            message:'Please provide testing instructions for your project.'
+        },
+        {
+            type: 'input',
+            name:'email',
+            message:'What is your email?'
+        },
+        {
+            type: 'input',
+            name:'username',
+            message:'What is your GitHub username?'
+        },
+        {
+            type: 'input',
+            name:'repository',
+            message:'What is the link to your GitHub repository?'
+        },
+
+    ]).then((input) => {
+        // generate markdown for input
+        const message = generateMarkdown(input)
+        fs.writeFileSync('./output/readme.md', message, (err) => err? console.log(err): console.log(generateReadme));
+        console.log(input)
+
+    }).catch(function(){
+        console.log('promise rejected')
+    })
+};
+// call to initialize app
+    const init = () => {
+    promptUser()
+    };
+
+init();
---
+++ util/generateMarkdown.js
@@ -0,0 +1,54 @@
+// generate markdown for the readme
+
+const generateReadme = (answers) =>
+ `
+## ${answers.title}
+
+
+
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributors](#contributors)
+- [Testing](#testing)
+- [Questions](#questions)
+
+
+## Installation
+${answers.installation}
+
+
+## Usage
+${answers.usage}
+
+
+## License
+![badge](https://img.shields.io/badge/license-${answers.license}-red)
+
+
+## Contributors
+${answers.contributors}
+
+
+## Testing
+${answers.questions}
+
+
+## Questions
+If you have any questions, please contact me below:
+
+GitHub: [${answers.username}](https://github.com/${answers.username})
+
+Email:  [${answers.email}](mailto:${answers.email})
+
+
+`;
+
+module.exports = generateReadme;
+
